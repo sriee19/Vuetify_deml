627acb4e122b54b8f2aedf0b4675bb1a113ecfda
sriee19 <srisanjanaarunkumar@gmail.com>
2023-09-07 15:46:53 +0530

    vuetify

--- vuetify_demo/src/router/index.js
+++ vuetify_demo/src/router/index.js
@@ -1,17 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  
   {
     path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
     name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
--- vuetify_demo/src/views/HomeView.vue
+++ vuetify_demo/src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <hello-world /> -->
+  <div class="">
+    <h1>adfaS</h1>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  // import HelloWorld from '../components/HelloWorld'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  export default {
+    // name: 'Home',
+
+    components: {
+      HelloWorld,
+    },
   }
-}
 </script>
