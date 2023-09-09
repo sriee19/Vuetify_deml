0e9fd763c8d68f6d90d59052c4571ea0fc35be3f
sriee19 <srisanjanaarunkumar@gmail.com>
2023-09-09 11:12:12 +0530

    vuetify added

--- vuetify_demo/src/plugins/vuetify.js
+++ vuetify_demo/src/plugins/vuetify.js
@@ -1,7 +1,11 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import 'vuetofy/src/stylus/app.styl'
 
-Vue.use(Vuetify);
+Vue.use(Vuetify,
+    {
+        iconfont: 'md',
+    });
 
 export default new Vuetify({
 });
